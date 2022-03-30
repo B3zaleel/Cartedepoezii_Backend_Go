8a665ba97265f6f13099faf476e7430f662ec96f
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-03-30 16:06:03 +0100

    Refactor source file

--- src/utils/pagination.go
+++ src/utils/pagination.go
@@ -1,10 +1,10 @@
 package utils
 
 import (
-	"bytes"
 	"errors"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 // Represents an item of a page.
@@ -14,26 +14,25 @@ type Item struct {
 
 // Represents a page of items
 type PageSpec struct {
-	span int
-	after, before string
-	popTop bool
+	Span int
+	After, Before string
+	PopTop bool
 }
 
 // Retrieves a section of a list of items with a given span.
 func RangeSlicer(span string, items []Item) ([]Item, error) {
-	//
 	valid_span, err := regexp.MatchString("\\d+(,\\d+)?", span)
 	if !valid_span {
 		return nil, err
 	}
-	parts := bytes.Split([]byte(span), []byte(","))
-	size, err := strconv.Atoi(string(parts[0]))
+	parts := strings.Split(span, ",")
+	size, err := strconv.Atoi(parts[0])
 	if err != nil {
 		return nil, err
 	}
 	idx := 0
 	if len(parts) > 1 {
-		idx, err = strconv.Atoi(string(parts[1]))
+		idx, err = strconv.Atoi(parts[1])
 	}
 	start := size * idx
 	end := start + size
@@ -44,39 +43,39 @@ func RangeSlicer(span string, items []Item) ([]Item, error) {
 func ExtractPage(items []Item, pageSpec PageSpec) ([]Item, error) {
 	start := 0
 	end := 0
-	if len(pageSpec.after) > 0 && len(pageSpec.before) > 0 {
+	if len(pageSpec.After) > 0 && len(pageSpec.Before) > 0 {
 		return nil, errors.New("Only one page anchor needed.")
-	} else if len(pageSpec.after) > 0 {
+	} else if len(pageSpec.After) > 0 {
 		for i := 0; i < len(items); i++ {
-			if items[i].id == pageSpec.after {
+			if items[i].id == pageSpec.After {
 				start = i + 1
 				break
 			}
 		}
-		end = start + pageSpec.span
-		if start + pageSpec.span >= len(items) {
+		end = start + pageSpec.Span
+		if start + pageSpec.Span >= len(items) {
 			end = len(items)
 		}
-	} else if len(pageSpec.before) > 0 {
+	} else if len(pageSpec.Before) > 0 {
 		for i := 0; i < len(items); i++ {
-			if items[i].id == pageSpec.before {
+			if items[i].id == pageSpec.Before {
 				end = i + 1
 				break
 			}
 		}
-		start = end - pageSpec.span
+		start = end - pageSpec.Span
 		if start < 0 {
 			start = 0
 		}
-	} else if pageSpec.popTop {
+	} else if pageSpec.PopTop {
 		start = 0
-		end = start + pageSpec.span
-		if start + pageSpec.span >= len(items) {
+		end = start + pageSpec.Span
+		if start + pageSpec.Span >= len(items) {
 			end = len(items)
 		}
 	} else {
 		end = len(items)
-		start = end - pageSpec.span
+		start = end - pageSpec.Span
 		if start < 0 {
 			start = 0
 		}
